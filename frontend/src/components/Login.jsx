b5835f301b962a885d91e5e79360ca4e64f60a7d
miguelonmiguelon <miguelcatholic@gmail.com>
2026-04-29 22:30:15 -0500

    subiendo correciones formulario

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -16,15 +16,52 @@ const Login = () => {
     e.preventDefault();
     alert("Inicio de sesión exitosa")
   }
+
+  const validateUsername = (username) => {
+    const withoutSpaces = username.trim();
+    if(withoutSpaces.length > 2){
+      return true
+    } else {
+      return false;
+    }
+  }
+
+  const validatePassword = (password) =>{
+      const withoutSpaces = password.trim();
+      const passwordAsArray = password.split("");
+      const hasNumber = passwordAsArray.some((character)=>{
+        if(isNaN(character)){
+          return false;
+        } else {
+          return true;
+        }
+      })
+      if(withoutSpaces .length > 5 && hasNumber){
+        return true;
+      }else{
+        return false;
+      }
+  }
+  const onSubmitForm = (e) =>{
+    e.preventDefault();
+    const isUsernameValid = validateUsername(formData.username);
+    const isPasswordValid = validatePassword(formData.password);
+
+    if(!isUsernameValid || !isPasswordValid){
+      alert("Usuario o contraseña incorrectos!!!")
+    } else{
+      alert("Incio de sesión exitosamente!!!")
+    }
+  }
     
   return (
     <div>
       <Header/>
-      <form onSubmit={handleSubmit} className='formulario'>
+      <form onSubmit={onSubmitForm} className='formulario'>
         <label htmlFor="">Usuario</label>
-        <input type="text" name='username' value={formData.username}/>
+        <input type="text" name='username' value={formData.username} onChange={handleChange}/>
         <label htmlFor="">Contraseña</label>
-        <input type="password" name='password' value={formData.password}/>
+        <input type="password" name='password' value={formData.password} onChange={handleChange}/>
         <input type="submit" />
       </form>
     </div>
--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -19,21 +19,71 @@ const SignUp = () => {
     }
 
 
-    function handleSubmit (e) {
-        e.preventDefault();
-        alert("Registro exitoso!!")
+    
+
+    const validateEmail = (email) =>{
+     
+      const withoutSpaces = email.trim();
+        if(withoutSpaces.length > 2 && withoutSpaces.includes("@")){
+          return true
+        }else{
+          return false
+        }
     }
+
+    const validateUsername = (username) =>{
+      const withoutSpaces = username.trim();
+      if(withoutSpaces.length > 2){
+        return true
+      }else{
+        return false;
+      }
+    }
+
+    const validatePassword = (password) =>{
+      const withoutSpaces = password.trim();
+      const passwordAsArray = withoutSpaces.split("");
+      const hasNumber = passwordAsArray.some((character)=>{
+        if(isNaN(character)){
+          return false
+        }else{
+          return true;
+        }
+      })
+      if(withoutSpaces.length > 5 && hasNumber){
+        return true
+      }else{
+        return false;
+      }
+    }
+
+    const onSubmitForm = (e) =>{
+      e.preventDefault();
+
+      const isValidEmail = validateEmail(formData.email);
+      const isValidUsername = validateUsername(formData.username);
+      const isValidPassword = validatePassword(formData.password);
+
+      if(!isValidEmail || !isValidUsername || !isValidPassword){
+        alert("Correo, usuario o contraseña incorrectos!")
+      } else{
+        alert("Registro exitoso!!!")
+      }
+    }
+
+
+
   return (
     <div>
       <Header/>
-      <form onSubmit={handleSubmit} className="formulario">
+      <form onSubmit={onSubmitForm} className="formulario">
         <label htmlFor="">Nombre: </label>
         <input type="text" name="name" value={formData.name} onChange={handleChange} />
         <label htmlFor="">Usuario</label>
         <input type="text" name="username" value={formData.username}
         onChange={handleChange} />
         <label htmlFor="">Correo</label>
-        <input type="text" name="email" value={formData.email}
+        <input type="email" name="email" value={formData.email}
         onChange={handleChange} />
         <label htmlFor="">Contraseña</label>
         <input type="password" name="password" value={formData.password}
