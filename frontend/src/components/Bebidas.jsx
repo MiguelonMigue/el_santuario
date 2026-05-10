dffdec17aff9c7d7071e1844b452e9cd87ea1382
miguelonmiguelon <miguelcatholic@gmail.com>
2026-05-10 14:56:59 -0500

    subiendo postres

--- frontend/src/components/Bebidas.jsx
+++ frontend/src/components/Bebidas.jsx
@@ -1,9 +1,82 @@
 import Header from "./Header"
+import Footer from './Footer'
 const Bebidas = () => {
+  const jugos = [{
+    id: 1,
+    sabor: "Mora"
+  },{
+    id: 2,
+    sabor: "Maracuyá"
+  },
+  {
+    id: 3,
+    sabor: "Lulo"
+  },
+  {
+    id: 4,
+    sabor: "Mango"
+  },
+  {
+    id: 5,
+    sabor: "Limonada de panela"
+  },{
+    id: 6,
+    sabor: "Tomate de árbol"
+  },{
+    id: 7,
+    sabor: "Piña"
+  }
+]
+const cocaColas =[
+  {
+    id: 1,
+    tipo: "Normal"
+  },{
+    id: 2,
+    tipo: "Zero"
+  }
+]
+const aguas =[
+  {
+    id: 1,
+    tipo: "Sin gas",
+    presentacion: "Botella azul"
+  },{
+    id: 2,
+    tipo: "Con gas",
+    presentacion: "Botella verde"
+  }
+]
+const cervezas = [{id: 1, marca: "Club Colombia"}, {id: 2, marca: "Andina"}]
   return (
     <div>
         <Header/>
-      Bebidas
+      <h1 className="titulo_principal">De bebidas le ofrezco</h1>
+      <h2 className="titulo_secundario">Jugos</h2>
+      {jugos.map((jugo)=>(
+        <div key={jugo.id}>
+          <p>{jugo.sabor}</p>
+        </div>
+      ))}
+      <h2 className="titulo_secundario">Aguas</h2>
+      {aguas.map((agua)=>(
+        <div key={agua.key}>
+          <p>{agua.tipo}, viene en {agua.presentacion}</p>
+        </div>
+      ))}
+      <h2 className="titulo_secundario">Coca Cola en lata</h2>
+      {cocaColas.map((cocaCola)=>(
+        <div key={cocaCola.id}>
+          <p>{cocaCola.tipo}</p>
+        </div>
+      ))}
+      <h2 className="titulo_secundario">Cerveza en lata</h2>
+      {cervezas.map((cerveza)=>(
+        <div key={cerveza.id}>
+          <p>{cerveza.marca}</p>
+        </div>
+      ))}
+      <Footer/>
     </div>
   )
 }
--- frontend/src/components/Postres.jsx
+++ frontend/src/components/Postres.jsx
@@ -1,9 +1,42 @@
 import Header from "./Header"
+import Footer from "./Footer"
 const Postres = () => {
+  const postrecitos =[{
+    id: 1,
+    nombre: "Arroz con leche",
+    precio: "$5.0k"
+  },{
+    id: 2,
+    nombre: "Postre de limón",
+    precio: "$6.0k"
+  },{
+    id: 3,
+    nombre: "Merenguitos",
+    precio: "$4.0k"
+  },{
+    id:4,
+    nombre:"Brazo de reina",
+    precio:"$8.0k"
+  },{
+    id: 5,
+    nombre:"Milhoja",
+    precio: "$8.0k"
+  },{
+    id:6,
+    nombre: "Matrimonio santuario",
+    precio: "8.0k"
+  }]
   return (
     <div>
         <Header/>
-      Postres
+        <h1>¡¡¡De postres le ofrezco!!!</h1>
+        {postrecitos.map((postrecito)=>(
+          <div key={postrecito.id}>
+            <h2 className="titulo_secundario">{postrecito.nombre}</h2>
+            <p>{postrecito.precio}</p>
+          </div>
+        ))}
+       <Footer/>
     </div>
   )
 }
